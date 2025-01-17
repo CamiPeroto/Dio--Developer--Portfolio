e0b5dd54e3d8c4b20d9a1bbfd96e580bbf0432d7
Caperoto <camila.peroto@icloud.com>
2025-01-16 22:42:17 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/CamiPeroto/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/CamiPeroto/Dio--Developer--Portfolio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
